654a546f92a579905466fbb45281f1f9f57da2bb
Sahaj Jain <jainsahaj@gmail.com>
2020-07-10 23:30:27 +0530

    Added relationships and groups

--- resources/freeroam/client.mjs
+++ resources/freeroam/client.mjs
@@ -2,8 +2,10 @@ import game from 'natives';
 
 game.setPedDefaultComponentVariation(game.playerPedId());
 
-let myped = game.createRandomPed(10, 2, 72);
-
-game.taskGoToEntity(myped,game.getPlayerPed(-1),-1, 0.0, 10.0, 1073741824.0, 0); 
-
-game.setPedCombatMovement(myped,3);
+game.addRelationshipGroup("rioters");
+var myped = game.createRandomPed(10, 2, 72);
+game.setPedCombatAttributes(myped, 46, true);
+game.setPedRelationshipGroupHash(myped, game.getHashKey("rioters"));
+game.setRelationshipBetweenGroups(5, game.getHashKey("rioters"), game.getHashKey("PLAYER"));
+game.setRelationshipBetweenGroups(5, game.getHashKey("PLAYER"), game.getHashKey("rioters"));
+game.taskGoToEntity(myped,game.getPlayerPed(-1),-1, 1.0, 10.0, 1073741824.0, 0);
